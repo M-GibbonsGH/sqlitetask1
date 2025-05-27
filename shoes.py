73f3ec293754f9f54229234df53eab3d4a484fc3
Matthew Gibbons <matthew.arthurgibbons@gmail.com>
2025-05-27 17:33:15 +0100

    Add another recursive cte

--- shoes.py
+++ shoes.py
@@ -17,7 +17,7 @@ cursor = conn.cursor()                                       # To modify the tab
 # Below, inserting 20 new rows into the table, and populating a single column with the same value for each row. Recursive common table expression.
 
 cursor.execute("""WITH RECURSIVE rec_cte(n) AS (select 1 union all select n + 1 from rec_cte where n < 20) INSERT INTO shoes_table (Brand) SELECT "Topo" from rec_cte""")
-                                                        # IMPORTANT: two different syntaxes appear to work fine: 'instantiating' the cte can come after OR before the insert clause
+                                                        # IMPORTANT: two different syntaxes appear to work fine: 'instantiating' the cte can come before OR after the insert clause
 
 conn.commit()
 
@@ -38,15 +38,21 @@ conn.commit()
 # Statement == one SQL command
 # Transaction == grouping of multiple SQL statments that sqlite will treat as a single unit
 
-# seems that you cannot have Offset by itself: must be limit...offset, otherwise error 
+# seems that you cannot have Offset by itself: must be Limit...Offset, otherwise error 
 cursor.execute("UPDATE shoes_table SET Model = 'Ultrafly' WHERE ROWID IN (SELECT ROWID FROM shoes_table WHERE BRAND = 'Topo' LIMIT 10 OFFSET 10)");
                
 cursor.execute("UPDATE shoes_table SET Sex = 'Female' WHERE ROWID IN (SELECT ROWID FROM shoes_table WHERE Brand = 'Topo' LIMIT 5 OFFSET 10)");
                
 cursor.execute("UPDATE shoes_table SET Sex = 'Male' WHERE ROWID IN (SELECT ROWID FROM shoes_table WHERE Brand = 'Topo' LIMIT 5 OFFSET 15)");
 
-               
+
+cursor.execute("WITH RECURSIVE rec_cte2(x) AS (SELECT 1 UNION ALL SELECT x + 1 FROM rec_cte2 WHERE x < 30) INSERT INTO shoes_table (Brand) SELECT 'True Motion' from rec_cte2")
+
 conn.commit()
 
-q = pd.read_sql_query("SELECT * FROM shoes_table WHERE Brand = 'Topo'", conn)
+q = pd.read_sql_query("SELECT * FROM shoes_table", conn)
 print(q)
+
+conn.close()
+
+# so next to find out: why are the added rows not indexed? They should be
